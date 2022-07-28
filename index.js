91a4e11d1ccb8a9a97070db498aa8d9c21402690
Andrew Tretyakov <drobservis@gmail.com>
2022-07-28 21:15:40 +0300

    add weather

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ const setBg = () => {
     randomNum < 10 ? bgNum = "0" + randomNum : bgNum = randomNum.toString();
     const img = new Image();
     img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
-    // const bgLink = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
     img.addEventListener('load', () => {
         body.style.backgroundImage = `url(${img.src})`;
     })
@@ -93,9 +92,68 @@ const getSlidePrev = () => {
 prevSlide.addEventListener('click', getSlidePrev);
 nextSlide.addEventListener('click', getSlideNext);
 
-
 getRandomNum();
 setUserName();
 setBg();
 showTime();
 showGreeting();
+
+
+
+//WEATHER//
+// TODO: выводится уведомление об ошибке при вводе некорректных значений, 
+// для которых API не возвращает погоду (пустая строка или бессмысленный набор символов) +5
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+
+if (localStorage.getItem('city')) {
+    city.value = localStorage.getItem('city');
+} else {
+    city.value = 'Minsk';
+}
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=89274f8372c411e6c202134024e9c1a5&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setCityLocalStorage();
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity.toFixed(0)}%`;
+}
+
+function setCity(event) {
+    if (event.code === 'Enter') {
+        getWeather();
+        city.blur();
+    }
+    getWeather();
+}
+
+function setCityLocalStorage() {
+    localStorage.setItem('city', city.value);
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
+city.addEventListener('change', setCity);
+
+
+
+//quotes
+async function getQuotes() {  
+    const quotes = 'data.json';
+    const res = await fetch(quotes);
+    const data = await res.json(); 
+    console.log(data);
+  }
+getQuotes();
